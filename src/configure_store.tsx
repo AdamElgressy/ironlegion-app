a730325faad34fd16ef95aa862d5f97b27d9713c
mageer <mageer.github@gmail.com>
2021-02-20 21:00:31 +0200

    feat: Redux-Saga
    Added redux-saga middleware to redux store and added saga directory.

--- src/configure_store.tsx
+++ src/configure_store.tsx
@@ -1,7 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers/";
 
+const sagaMiddleware = createSagaMiddleware();
+
 export default function configureStore() {
-  return createStore(rootReducer, composeWithDevTools());
+  return createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  );
 }
