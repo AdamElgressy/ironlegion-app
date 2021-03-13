c0122a0247b56867c0e7796d986cb2d699e670c0
mageer <mageer.github@gmail.com>
2021-03-13 16:34:54 +0200

    Trying Out WebSockets

---
+++ src/components/Socket.tsx
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from "uuid";
+
+const useSocket = (uuid: string) => {
+  const WS_URL = process.env.REACT_APP_WS_URL;
+  if (!WS_URL) {
+    throw new Error("API URI not set");
+  }
+
+  const ws = new WebSocket(WS_URL + "/client");
+  ws.onopen = () => {
+    console.log("opening...");
+    ws.send("hello server");
+  };
+  ws.onerror = (error) => {
+    console.log("WebSocket error " + error);
+    console.dir(error);
+  };
+  ws.onmessage = (message) => console.log(message.data);
+
+  return ws;
+};
+
+const clientId = uuidv4();
+
+const Socket = () => {
+  const ws = useSocket(clientId);
+  //ws.send("some string");
+  return (
+    <>
+      <button onClick={() => console.log("hey")}>Send</button>
+    </>
+  );
+};
+
+export default Socket;
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import { Provider } from "react-redux";
 import configureStore from "./configure_store";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
-import App from "./components/App";
+//import App from "./components/App";
+import Socket from "./components/Socket";
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <Socket />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
